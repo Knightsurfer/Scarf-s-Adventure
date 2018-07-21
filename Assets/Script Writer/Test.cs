b3842e6d8971c8b655178e7519ec94f193ee6e3d
Knightsurfer <armstrong5454@msn.com>
2018-07-21 14:50:13 +0100

    Created a script writer
    For those who don't like  opening a script editor I made a simple editor for use in game.

---
+++ Assets/Script Writer/Test.cs
@@ -0,0 +1,21 @@
+using UnityEngine;                               
+public class Test : MonoBehaviour  
+{                                                
+                                                 
+
+       void Start()                              
+       {                                         
+                                                 
+       }                                         
+                                                 
+                                                 
+                                                 
+       void Update()                             
+       {                                         
+                                                 
+       }                                         
+                                                 
+                                                 
+                                                 
+
+}                                                  
